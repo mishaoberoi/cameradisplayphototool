14f5e9c2644faf6c0dcc11510b2a6b2d9db4e44e
Misha <mitho.oberoi@gmail.com>
2022-06-14 22:56:00 +0530

    Initial Commit

---
+++ cameradisplayphototool/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  cameradisplayphototool
+//
+//  Created by sns on 14/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+      
+    }
+    @IBAction func imagePressed(_ sender: Any) {
+        
+        imagePicker.sourceType = .camera
+    }
+    
+    @IBAction func choosePhotoTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present (imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
